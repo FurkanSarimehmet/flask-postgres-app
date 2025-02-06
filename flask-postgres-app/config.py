1d0bed4f91d3d6007440686197060ada26a6758a
FurkanSarimehmet <93478096+FurkanSarimehmet@users.noreply.github.com>
2025-02-06 13:11:00 +0300

    Delete flask-postgres-app directory

--- flask-postgres-app/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://myuser:mypassword@localhost:5432/mydatabase")
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
