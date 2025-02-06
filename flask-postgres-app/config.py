d7cba3c6a778837bfc1c7b65c6ac2ef2fc0faf40
FurkanSarimehmet <furkansarimehmet37@gmail.com>
2025-02-06 12:58:12 +0300

    Add .gitignore file

---
+++ flask-postgres-app/config.py
@@ -0,0 +1,6 @@
+import os
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://myuser:mypassword@localhost:5432/mydatabase")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
